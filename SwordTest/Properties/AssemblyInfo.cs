52f3053f8921594e44f94e55b2b062cb272ff2de
M-Rob <35639667+M-Rob@users.noreply.github.com>
2018-01-20 21:26:24 +0000

    Add files via upload
    Initial Commit

---
+++ SwordTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SwordTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SwordTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53245fe1-13a8-43e2-a3cd-82d408fab06d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SwordTest/SearchTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support;
+using System.Linq;
+
+namespace UnitTestActiveRisk
+{
+    [TestClass]
+    public class MyTest
+    {
+        IWebDriver driverFF;
+
+        
+        /// <summary>
+        /// this test will search for the term Snappit via the search in the top right hand side
+        /// it will then search for the first appropriate link on the page and make sure the page from
+        /// that link will succesfully load.
+        /// </summary>
+        
+
+        [TestMethod]
+        public void CheckThatSearchReturnsLinkToSnappit()
+        {
+            var element = driverFF.FindElement(By.Id("s"));
+
+            element.SendKeys("snappit");
+
+            var elementid = driverFF.FindElement(By.Id("searchsubmit"));
+
+            elementid.Click();
+
+            var anchors = driverFF.FindElements(By.TagName("a"));
+
+            var readMoreAnchor = anchors.FirstOrDefault(a => a.GetAttribute("href") == "http://www.sword-activerisk.com/products/active-risk-manager-arm/arm-snappit/"
+                && a.Displayed == true); // Check that its displayed as there are other links to the same page that are hidden.
+
+            Assert.IsNotNull(readMoreAnchor);
+
+            readMoreAnchor.Click();
+
+            string title = driverFF.Title;
+
+            Assert.AreEqual("ARM SNAPPit", title);
+
+        }
+
+        [TestInitialize]
+        public void TestMethod1()
+        {
+            driverFF = new FirefoxDriver();
+            driverFF.Navigate().GoToUrl("http://www.sword-activerisk.com/");
+        }
+
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            driverFF.Quit();
+        }
+    }
+}
